968e53aa29bcc57e8adaabb7a850e6828ab2b9b9
Dawn inShadow <zpy18746442232@gmail.com>
2017-03-22 16:47:35 +0800

    咖啡商城

---
+++ HTML/Dawn/zpyJavaScript.js
@@ -0,0 +1,8 @@
+function changeimg(){
+    var pastpic = document.getElementById('past');
+    pastpic.src="./image/day.jpg";
+}
+function changeimg2(){
+    var pastpic = document.getElementById('past');
+    pastpic.src="./image/night.jpg";
+}
