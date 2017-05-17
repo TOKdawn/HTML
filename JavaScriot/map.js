14165e727dd12f37c8c8ff7ca86a4d068e7deb3e
Dawn inShadow <zpy18746442232@gmail.com>
2017-05-17 12:11:46 +0800

    垃圾JavaScript

--- JavaScriot/map.js
+++ JavaScriot/map.js
@@ -10,6 +10,12 @@ var BORDERUP=0;
 var BORDERLEFT=1;
 var BORDERBOTTEM=2;
 var BORDERRIGHT=3;
+var root1;
+var root2;
+var friend;
+var tree=new Array;
+var NUM=2399;
+var xxx;
 // function zpy() {
 //  var x;
 //  var y;
@@ -48,36 +54,132 @@ function draw(id) {
             context.fillRect(this.x,this.y,18,18);//绘制
             context.clearRect(this.x+3,this.y+3,12,12);//擦除路径对应的位置
         }
-        myArray[i].find=function (father) {
-            if(this.root<0)
-                return father;
-            else
-                return myArray.find(myArray[this.root].root)
-        }
+
         myArray[i].clearwall=function (border) {//拆除墙壁,参数border为要拆除的方向
             switch (border){
                 case BORDERUP:
                     context.clearRect(this.x+3,this.y+0,12,3);
                     break;
                 case BORDERLEFT:
-                    context.clearRect(this.x+15,this.y+3,3,12);
+                    context.clearRect(this.x+0,this.y+3,3,12);
                     break;
                 case BORDERBOTTEM:
                     context.clearRect(this.x+3,this.y+15,12,3);
                     break;
                 case BORDERRIGHT:
-                    context.clearRect(this.x+0,this.y+3,3,12);
+                    context.clearRect(this.x+15,this.y+3,3,12);
                     break;
             }
         }
         myArray[i].show();
     }
-
-    // while (1) {
-    //     rex = Math.random() * 1000;
-    //     rex = Math.floor(rex) % 2400;//取随机数
-    //     rey = rex % 4;//再生成一个0123中随机的方向随机数
-    //
+    // function shoaw(zzp) {
+    //     return myArray[zzp].root;
     // }
+
+    // function find(father) {
+    //     if(myArray[father].root<0)
+    //         return father;
+    //     else
+    //         return myArray[father].root;
+    // }
+
+    for(var j=0;j<2400;j++){
+        tree[j]=j;
+    }
+
+
+    for(var k=0;k<2400;k++){
+
+        // if((myArray[0].root > 0 )&& (myArray[0].root  = myArray[2399].root)){
+        //     break;
+        // }
+        //
+        xxx = (Math.random()) * NUM;
+        xxx = (Math.floor(xxx)) +1;//取随机数
+
+
+        rey = (Math.random()) * 10000;
+        rey = (Math.floor(rey))%4;//再生成一个0123中随机的方向随机数
+
+        rex=tree[xxx];
+        tree[xxx]=tree[NUM];
+
+
+
+
+        if(rex-60<0||rex+60>2399||(rex/MAPW)-1<0||(rex/MAPW)+1>59)//消除越界项目
+            continue;
+        NUM--;
+        root1=myArray[rex].root;//找当然单元的根
+
+        switch (rey){//找相邻单元的根
+            case 0:
+                friend=rex-60;
+                break;
+            case 1:
+                friend=rex+1;
+                break;
+            case 2:
+                friend=rex+60;
+                break;
+            case 3:
+                friend=rex-1;
+                break;
+        }
+        root2=myArray[friend].root;
+        if((root1==-1)&& (root2==-1)){//根据单元和相邻单元的位置拆墙
+                myArray[friend].root=NUM;
+                myArray[rex].root=NUM;
+            switch (rey){
+                case 0:
+                    myArray[rex].clearwall(BORDERUP);
+                    myArray[friend].clearwall(BORDERBOTTEM);
+                    break;
+                case 1:
+                    myArray[rex].clearwall(BORDERRIGHT);
+                    myArray[friend].clearwall(BORDERLEFT);
+                    break;
+                case 2:
+                    myArray[rex].clearwall(BORDERBOTTEM);
+                    myArray[friend].clearwall(BORDERUP);
+                    break;
+                case 3:
+                    myArray[rex].clearwall(BORDERLEFT);
+                    myArray[friend].clearwall(BORDERRIGHT);
+                    break;
+            }
+            continue;
+        }
+        if(root2!=root1){
+            for(var count=0;count<2400;count++){
+                if(myArray[count].root==root1||myArray[count].root==root2) {
+                    myArray[count].root = NUM;
+                }
+            }
+
+            switch (rex){
+                case 0:
+                    myArray[rex].clearwall(BORDERUP);
+                    myArray[friend].clearwall(BORDERBOTTEM);
+                    break;
+                case 1:
+                    myArray[rex].clearwall(BORDERRIGHT);
+                    myArray[friend].clearwall(BORDERLEFT);
+                    break;
+                case 2:
+                    myArray[rex].clearwall(BORDERBOTTEM);
+                    myArray[friend].clearwall(BORDERUP);
+                    break;
+                case 3:
+                    myArray[rex].clearwall(BORDERLEFT);
+                    myArray[friend].clearwall(BORDERRIGHT);
+                    break;
+            }
+            continue;
+        }
+        continue;
+
+    }
 
 }
