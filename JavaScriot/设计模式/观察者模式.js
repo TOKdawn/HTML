44f77cbc650935dcb53e52b127ca0c8861081789
Zia <sansroman@foxmail.com>
2017-09-26 14:44:58 +0800

    modify
    add observer and subject class,improve prototype(Self think)

--- JavaScriot/设计模式/观察者模式.js
+++ JavaScriot/设计模式/观察者模式.js
@@ -1,7 +1,56 @@
+function Subject() {
+    this.data = new Object();
+    this.observers = new Array();
+}
+Subject.prototype.registerObserver = function(o) {
+    this.observers.push(o);
+}
+Subject.prototype.removeObserver = function(o) {
+    this.observers.remove(o);
+}
+Subject.prototype.notifyObserver = function() {
+    for (let o in this.observers)
+        this.observers[o].update(this.data);
+
+}
+Subject.prototype.setData = function(data) {
+    this.data = data;
+    this.dataChanged();
+}
+Subject.prototype.dataChanged = function() {
+    this.notifyObserver();
+}
+
 function Observer() {
+    this.data = new Object();
+}
+Observer.prototype.update = function(data) {
+    this.data = data;
+    this.show();
+}
+Observer.prototype.show = function() {
+    for (let index in this.data)
+        console.log(this.data[index]);
+}
+
+
+let subject = new Subject();
+let observer1 = new Observer();
+let observer2 = new Observer();
+subject.registerObserver(observer1);
+subject.registerObserver(observer2);
+subject.setData({ id: 12, name: "one" });
+subject.setData({ id: 14, name: "two" });
+
+
+
+
+
+//second version 
+function Subject() {
     this.fns = [];
 }
-Observer.prototype = {
+Subject.prototype = Object.assign({
     subscribe: function(fn) {
         this.fns.push(fn);
     },
@@ -14,33 +63,50 @@ Observer.prototype = {
             }
         );
     },
-    update: function(o, thisObj) {
-        var scope = thisObj;
-        console.log(o);
-        this.fns.forEach(
-            function(el) {
-                el.call(scope, o);
-            }
-        );
+    notify: function(message) {
+        for (let o in this.fns)
+            this.fns[o].update(message);
     }
-};
+}, Subject.prototype);
+
+
+function Observer() {
+    this.message = {}
+}
+Observer.prototype.update = function(message) {
+    this.message = message;
+    this.show(message);
+}
+Observer.prototype.show = function() {
+    console.log(this.message.code);
+    console.log(this.message.text);
+}
 
 //测试
-var o = new Observer;
-var f1 = function(data) {
-    console.log('Robbin: ' + data + ', 赶紧干活了！');
-};
+let o = new Subject;
+let f1 = new Observer;
 
-var f2 = function(data) {
-    console.log('Randall: ' + data + ', 找他加点工资去！');
-};
+let f2 = new Observer;
+//自定义回调函数
+f2.update = function(message) {
+    console.log("我收到了消息，但我只显示代码：" + message.code);
+}
 
 o.subscribe(f1);
 o.subscribe(f2);
+o.notify({
+    code: 404,
+    text: "not found"
+});
 
-o.update("Tom回来了！", 10)
 
-//退订f1
+o.notify({
+    code: 200,
+    text: "OK"
+});
+
 o.unsubscribe(f1);
-//再来验证
-o.update("Tom回来了！");
+o.notify({
+    code: 304,
+    text: "重定向"
+});
